22a78fa908c01ef0567b14b89554bbbd5cae6fdf
Evan Sullivan <20093137@mail.wit.ie>
2024-01-15 20:29:10 +0000

    added firebase to project

--- app/src/main/java/com/wit/assignment1/Activities/RegisterActivity.kt
+++ app/src/main/java/com/wit/assignment1/Activities/RegisterActivity.kt
@@ -2,11 +2,23 @@ package com.wit.assignment1.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.wit.assignment1.R
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var useremail: EditText
+    private lateinit var userpass: EditText
+    private lateinit var btnRegister: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        useremail = findViewById(R.id.userEmail)
+        userpass = findViewById(R.id.userPassword)
+        btnRegister = findViewById(R.id.RegisterBtn)
+
+
     }
 }
